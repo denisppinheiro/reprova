8d50f304991ccaa9d5789741298240f141ebeedb
Denis Pinheiro <denisppinheiro@gmail.com>
2021-03-06 20:23:31 -0300

    (Java) Standard outputs should not be used directly to log anything

--- src/java/br/ufmg/engsoft/reprova/Reprova.java
+++ src/java/br/ufmg/engsoft/reprova/Reprova.java
@@ -1,5 +1,8 @@
 package br.ufmg.engsoft.reprova;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.ufmg.engsoft.reprova.database.AnswersDAO;
 import br.ufmg.engsoft.reprova.database.Mongo;
 import br.ufmg.engsoft.reprova.database.QuestionsDAO;
@@ -8,34 +11,36 @@ import br.ufmg.engsoft.reprova.routes.Setup;
 import br.ufmg.engsoft.reprova.mime.json.Json;
 import br.ufmg.engsoft.reprova.model.Environments;
 
-
 public class Reprova {
-  public static void main(String[] args) {
-    var json = new Json();
-    
-    Mongo db;
 
-    try {
-    	db = new Mongo("reprova");
-    } catch(Exception e) {
-    	System.out.println(e);
-    	return;
-    }
+	private static final Logger logger = LoggerFactory.getLogger(Reprova.class);
 
-    var questionsDAO = new QuestionsDAO(db, json);    
+	public static void main(String[] args) {
+		var json = new Json();
 
-    Setup.routes(json, questionsDAO);
-    
-    Environments envs = Environments.getInstance();
-    
-    if (envs.getEnableAnswers()) {
-        var answersDAO = new AnswersDAO(db, json);
-        Setup.answerRoutes(json, answersDAO);
-    }
+		Mongo db;
 
-    if (envs.getEnableQuestionnaires()) {
-        var questionnairesDAO = new QuestionnairesDAO(db, json);
-        Setup.questionnaireRoutes(json, questionnairesDAO, questionsDAO);
-    }
-  }
+		try {
+			db = new Mongo("reprova");
+		} catch (Exception e) {
+			logger.error("Error on bootstrap Mongo DB.", e);
+			return;
+		}
+
+		var questionsDAO = new QuestionsDAO(db, json);
+
+		Setup.routes(json, questionsDAO);
+
+		Environments envs = Environments.getInstance();
+
+		if (envs.getEnableAnswers()) {
+			var answersDAO = new AnswersDAO(db, json);
+			Setup.answerRoutes(json, answersDAO);
+		}
+
+		if (envs.getEnableQuestionnaires()) {
+			var questionnairesDAO = new QuestionnairesDAO(db, json);
+			Setup.questionnaireRoutes(json, questionnairesDAO, questionsDAO);
+		}
+	}
 }
